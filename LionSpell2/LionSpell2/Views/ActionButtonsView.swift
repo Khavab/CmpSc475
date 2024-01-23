e0d9025edf99eeb28588e4a766db3f49ff2fcaad
Khavab <shiblidnomani@gmail.com>
2024-01-23 11:00:20 -0500

    make visibly clear button is disabled

--- LionSpell2/LionSpell2/Views/ActionButtonsView.swift
+++ LionSpell2/LionSpell2/Views/ActionButtonsView.swift
@@ -34,11 +34,12 @@ struct ActionButtonsView: View {
 
 struct StyledButton: ButtonStyle {
     let backgroundColor: Color
+    @Environment(\.isEnabled) var isEnabled: Bool
 
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
             .padding()
-            .background(backgroundColor)
+            .background(isEnabled ? backgroundColor : backgroundColor.opacity(0.5))
             .foregroundColor(.white)
             .cornerRadius(10)
             .scaleEffect(configuration.isPressed ? 0.95 : 1)
