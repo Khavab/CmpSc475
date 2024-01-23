8833f7ca962c394fd9c9b9524b9600ed1cfcb0a8
Khavab <shiblidnomani@gmail.com>
2024-01-23 01:32:59 -0500

    Initial LionSpell II Commit

---
+++ LionSpell2/LionSpell2/LionSpell2App.swift
@@ -0,0 +1,17 @@
+//
+//  LionSpell2App.swift
+//  LionSpell2
+//
+//  Created by Shibli Nomani on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LionSpell2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
