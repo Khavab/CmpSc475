21509aa358a59855449cd91f3cca6af96e3970db
Khavab <shiblidnomani@gmail.com>
2024-01-24 14:07:33 -0500

    use only one project

---
+++ LionSpell/LionSpell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LionSpell
+//
+//  Created by Shibli Nomani on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
