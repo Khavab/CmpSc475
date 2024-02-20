5eae8043bbf8850abd58e0a05216d55136ecdfe5
Khavab <shiblidnomani@gmail.com>
2024-02-20 08:12:14 -0500

    implement deselect

---
+++ Campus/Campus/Views/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  Campus
+//
+//  Created by Shibli Nomani on 2/20/24.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
